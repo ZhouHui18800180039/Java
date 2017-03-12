f5b4d10b9053e5b9ce05e8fab3f547f4273ff4c6
ZhouHui18800180039 <786126222@qq.COM>
2017-03-12 22:17:21 +0800

    add permutation

--- permutation.java
+++ permutation.java
@@ -1,13 +1,42 @@
-/*
-编写一个程序，输出字符串"abcdefgh"的全排列。（提示：可考虑递归）
-*/
-public class permutation{
-	public static void main(String[] args){
-		char m;
-		char[] chars={'a','b','c'};
+
+public class permutation {
+	public static void main(String[] args) {
 		
-                //打印chars的全排列		
-		permutate(chars,0);
+		permutate("", "abcdefgh");
+		
+//		char[] chars = {'a', 'b', 'c','d'};
+//		permutate(chars, 0, chars.length);
 	}
-			
+	
+	public static void permutate(String prefix, String postfix) {
+		if(postfix.length() == 0) {
+			System.out.println(prefix);
+		} else {
+			for(int i=0; i<postfix.length(); i++) {
+				permutate(prefix + postfix.charAt(i),
+						  postfix.substring(0, i) + postfix.substring(i+1, postfix.length()));
+			}
+		}
+	}
+	
+	public static void permutate(char[] chars, int start, int end) {
+		char tmp;
+		if(start<end-1) {
+			permutate(chars, start+1, end);
+			for(int i=start+1; i<end; i++) {
+				tmp = chars[i];
+				chars[i] = chars[start];
+				chars[start] = tmp;
+				permutate(chars, start+1, end);
+				tmp = chars[i];
+				chars[i] = chars[start];
+				chars[start] = tmp;
+			}
+		} else {
+			for(char c: chars) {
+				System.out.print(c);
+			}
+			System.out.println();
+		}
+	}
 }
