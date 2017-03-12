06c10e3017d7d255d167613e18ead9bdea8c3445
ZhouHui18800180039 <786126222@qq.COM>
2017-03-12 22:21:01 +0800

    add sort

--- sort.java
+++ sort.java
@@ -1,15 +1,26 @@
-/*
-用Java编写一个能对数组比较的程序，需要输出按行升序排序的结果 如输入
-1 2 5 4 8
-输出：
-1 2 4 5 8
-要求：数组是通过sort程序的参数输入。
-*/
+
 public class sort{
-	 public static void main(String[] args){
-         //将args转换为int数组
-         
-         //对int数组排序后输出结果
-	 	 
+
+	public static void main(String[] args) {
+
+		//将args转换为int数组
+		int[] arr = new int[args.length];
+		for(int i=0; i<args.length; i++) {
+			arr[i] = Integer.parseInt(args[i]);
+		}
+		
+		//对int数组排序后输出结果
+		int tmp;
+		for(int i=0; i<arr.length; i++) {
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[i] < arr[j]) {
+					tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+			System.out.println(arr[i]);
+		}
 	}
+
 }
